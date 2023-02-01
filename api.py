097a3b31a06cc85e226f528158d33602b6c83cdf
Biagio <biagio.chirico.86@gmail.com>
2023-02-01 09:42:07 +0100

    add code for 'Historical prices'

--- api.py
+++ api.py
@@ -3,11 +3,32 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 SECRET_TOKEN = 'Basic secret_test_token' # embedded here only for exercise purpose
 
-stocks = [
+STOCKS = [
     {'price': 100.0, 'symbol': 'aapl'},
     {'price': 200.0, 'symbol': 'amzn'},
     {'price': 300.0, 'symbol': 'tsla'}
 ]
+ 
+PRICES = {
+    'AAPL': [
+        {
+            'date': '2023-01-31',
+            'price': '100'
+        },
+        {
+            'date': '2023-01-30',
+            'price': '99.86'
+        }
+    ]
+}
+
+def get_last_90_days_prices(ticker):
+    # make a request to a stock price provider
+    # here I will make a fake function that returns only 90 days prices
+    # response = requests.get(f'https://some-stock-price-api.com/{ticker}/history')
+    # data = response.json()
+
+    return PRICES[ticker]
 
 def check_auth(auth_header):
     if auth_header == SECRET_TOKEN:
@@ -17,10 +38,28 @@ def check_auth(auth_header):
 @app.route('/stocks', methods=['GET'])
 def get_stocks():
     auth_header = request.headers.get('Authorization')
-    print(auth_header)
     if not auth_header or not check_auth(auth_header):
         return jsonify({'error': 'Unauthorized'}), 401
-    return jsonify(stocks)
+    return jsonify(STOCKS)
+
+@app.route('/tickers/<string:ticker>/history', methods=['GET'])
+def get_stock_history(ticker):
+    auth_header = request.headers.get('Authorization')
+    if not auth_header or not check_auth(auth_header):
+        return jsonify({'error': 'Unauthorized'}), 401
+    
+    ticker = ticker.upper()
+
+    if ticker not in PRICES.keys():
+        return jsonify({'error': 'Invalid ticker'}), 404
+
+    data = get_last_90_days_prices(ticker)
+
+    # sort in date descending order
+    sorted_data = sorted(data, key=lambda x: x['date'], reverse=True)
+
+    return jsonify(sorted_data)
+
 
 if __name__ == '__main__':
     app.run()
--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import requests
 
 class TestAPI(unittest.TestCase):
     def setUp(self):
-        self.url = "http://localhost:5000/stocks"
-        self.headers = {"Authorization": "Basic secret_test_token"}
+        self.url = 'http://localhost:5000'
+        self.headers = {'Authorization': 'Basic secret_test_token'}
 
     def test_get_stocks_success(self):
         correct_data = [
@@ -12,14 +12,44 @@ class TestAPI(unittest.TestCase):
             {'price': 200.0, 'symbol': 'amzn'},
             {'price': 300.0, 'symbol': 'tsla'}
         ]
-        response = requests.get(self.url, headers=self.headers)
+        response = requests.get(f'{self.url}/stocks', headers=self.headers)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), correct_data)
 
     def test_get_stocks_unauthorized(self):
-        response = requests.get(self.url)
+        response = requests.get(f'{self.url}/stocks')
         self.assertEqual(response.status_code, 401)
-        self.assertEqual(response.json(), {"error": "Unauthorized"})
+        self.assertEqual(response.json(), {'error': 'Unauthorized'})
 
-if __name__ == "__main__":
+    def test_get_stock_history(self):
+        response = requests.get(f'{self.url}/tickers/AAPL/history', headers=self.headers)
+        data = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(data, list)
+
+    def test_get_stock_history_unauthorized(self):
+        response = requests.get(f'{self.url}/tickers/AAPL/history')
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {'error': 'Unauthorized'})
+
+    def test_get_stock_history_invalid_ticker(self):
+        response = requests.get(f'{self.url}/tickers/INVALID/history', headers=self.headers)
+        self.assertEqual(response.status_code, 404)
+    
+    def test_get_stock_history_date_descending_order(self):
+        response = requests.get(f'{self.url}/tickers/aapl/history', headers=self.headers)
+        correct_data = [
+            {
+                'date': '2023-01-31',
+                'price': '100'
+            },
+            {
+                'date': '2023-01-30',
+                'price': '99.86'
+            }
+        ]
+        self.assertEqual(response.json(), correct_data)
+
+
+if __name__ == '__main__':
     unittest.main()
